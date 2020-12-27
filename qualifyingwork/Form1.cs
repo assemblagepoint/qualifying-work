bfe69ddd612c5df703192ad0aaf9b76eeb0a14f7
assemblagepoint <61197901+assemblagepoint@users.noreply.github.com>
2020-12-27 17:44:08 +0400

    1

---
+++ qualifyingwork/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace qualifyingwork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Ввод выражения для записи в общую память
+            char[] message = textBox1.Text.ToCharArray();
+            //Размер введенного сообщения
+            int size = message.Length;
+
+            //Создание участка разделяемой памяти
+            //Первый параметр - название участка, 
+            //второй - длина участка памяти в байтах: тип char  занимает 2 байта 
+            //плюс четыре байта для одного объекта типа Integer
+            MemoryMappedFile sharedMemory = MemoryMappedFile.CreateOrOpen("MemoryFile", size * 2 + 4);
+            //Создаем объект для записи в разделяемый участок памяти
+            using (MemoryMappedViewAccessor writer = sharedMemory.CreateViewAccessor(0, size * 2 + 4))
+            {
+                //запись в разделяемую память
+                //запись размера с нулевого байта в разделяемой памяти
+                writer.Write(0, size);
+                //запись сообщения с четвертого байта в разделяемой памяти
+                writer.WriteArray<char>(4, message, 0, message.Length);
+            }        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Массив для сообщения из общей памяти
+            char[] message2;
+            //Размер введенного сообщения
+            int size2;
+
+            //Получение существующего участка разделяемой памяти
+            //Параметр - название участка
+            MemoryMappedFile sharedMemory = MemoryMappedFile.OpenExisting("MemoryFile");
+            //Сначала считываем размер сообщения, чтобы создать массив данного размера
+            //Integer занимает 4 байта, начинается с первого байта, поэтому передаем цифры 0 и 4
+            using (MemoryMappedViewAccessor reader = sharedMemory.CreateViewAccessor(0, 4, MemoryMappedFileAccess.Read))
+            {
+                size2 = reader.ReadInt32(0);
+            }
+
+            //Считываем сообщение, используя полученный выше размер
+            //Сообщение - это строка или массив объектов char, каждый из которых занимает два байта
+            //Поэтому вторым параметром передаем число символов умножив на из размер в байтах плюс
+            //А первый параметр - смещение - 4 байта, которое занимает размер сообщения
+            using (MemoryMappedViewAccessor reader = sharedMemory.CreateViewAccessor(4, size2 * 2, MemoryMappedFileAccess.Read))
+            {
+                //Массив символов сообщения
+                message2 = new char[size2];
+                reader.ReadArray<char>(0, message2, 0, size2);
+            }
+
+            string msg = new string(message2);
+            textBox2.Text = msg;
+            label2.Text = "Получено сообщение : " + msg;
+        }
+    }
+}
