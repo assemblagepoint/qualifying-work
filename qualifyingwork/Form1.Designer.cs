f31764b2cdf136c5db3ea6daed3fa9c3def51708
assemblagepoint <61197901+assemblagepoint@users.noreply.github.com>
2021-04-07 05:17:03 +0400

    тест отправки данных

--- qualifyingwork/Form1.Designer.cs
+++ qualifyingwork/Form1.Designer.cs
@@ -29,12 +29,10 @@ namespace qualifyingwork
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
@@ -47,32 +45,21 @@ namespace qualifyingwork
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 52);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(348, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Введите сообщение для отправки в общую память:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(383, 48);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Location = new System.Drawing.Point(118, 16);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(353, 22);
+            this.textBox1.Size = new System.Drawing.Size(266, 20);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(813, 45);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 30);
+            this.button1.Size = new System.Drawing.Size(100, 24);
             this.button1.TabIndex = 2;
             this.button1.Text = "Отправить";
             this.button1.UseVisualStyleBackColor = true;
@@ -81,104 +68,88 @@ namespace qualifyingwork
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(348, 144);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(118, 57);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(171, 17);
+            this.label2.Size = new System.Drawing.Size(130, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Полученное сообщение:";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(20, 137);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(12, 51);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(305, 30);
+            this.button2.Size = new System.Drawing.Size(100, 24);
             this.button2.TabIndex = 4;
-            this.button2.Text = "Получить сообщение из общей памяти:";
+            this.button2.Text = "Получить ";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(352, 164);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(353, 22);
-            this.textBox2.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(4, 455);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(3, 370);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(82, 17);
+            this.label3.Size = new System.Drawing.Size(64, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Параметр1";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(109, 455);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(82, 370);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(82, 17);
+            this.label4.Size = new System.Drawing.Size(64, 13);
             this.label4.TabIndex = 7;
             this.label4.Text = "Параметр2";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(8, 475);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Location = new System.Drawing.Point(6, 386);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(80, 22);
+            this.textBox3.Size = new System.Drawing.Size(61, 20);
             this.textBox3.TabIndex = 8;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(111, 475);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Location = new System.Drawing.Point(83, 386);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(80, 22);
+            this.textBox4.Size = new System.Drawing.Size(61, 20);
             this.textBox4.TabIndex = 9;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(8, 507);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Location = new System.Drawing.Point(6, 412);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(187, 30);
+            this.button3.Size = new System.Drawing.Size(140, 24);
             this.button3.TabIndex = 10;
             this.button3.Text = "Отправить в симуляцию";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(5, 315);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(4, 256);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(90, 17);
+            this.label5.Size = new System.Drawing.Size(70, 13);
             this.label5.TabIndex = 11;
             this.label5.Text = "Параметр1: ";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(128, 315);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(96, 256);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(90, 17);
+            this.label6.Size = new System.Drawing.Size(70, 13);
             this.label6.TabIndex = 12;
             this.label6.Text = "Параметр2: ";
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(8, 268);
-            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Location = new System.Drawing.Point(6, 218);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(184, 43);
+            this.button4.Size = new System.Drawing.Size(138, 35);
             this.button4.TabIndex = 13;
             this.button4.Text = "Получить значения из симуляции\r\n\r\n";
             this.button4.UseVisualStyleBackColor = true;
@@ -187,46 +158,55 @@ namespace qualifyingwork
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(224, 256);
+            this.label7.Location = new System.Drawing.Point(168, 208);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(160, 17);
+            this.label7.Size = new System.Drawing.Size(125, 13);
             this.label7.TabIndex = 14;
             this.label7.Text = "Размер общей памяти:";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(265, 315);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Location = new System.Drawing.Point(199, 256);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(90, 17);
+            this.label8.Size = new System.Drawing.Size(70, 13);
             this.label8.TabIndex = 15;
             this.label8.Text = "Параметр3: ";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(405, 315);
-            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Location = new System.Drawing.Point(304, 256);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(90, 17);
+            this.label9.Size = new System.Drawing.Size(70, 13);
             this.label9.TabIndex = 16;
             this.label9.Text = "Параметр4: ";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(224, 281);
+            this.label10.Location = new System.Drawing.Point(168, 228);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(131, 17);
+            this.label10.Size = new System.Drawing.Size(105, 13);
             this.label10.TabIndex = 17;
             this.label10.Text = "Размер заголовка:";
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(159, 386);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(61, 20);
+            this.textBox2.TabIndex = 18;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 550);
+            this.ClientSize = new System.Drawing.Size(800, 447);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
@@ -239,13 +219,10 @@ namespace qualifyingwork
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "application prototype";
             this.ResumeLayout(false);
@@ -254,13 +231,10 @@ namespace qualifyingwork
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox3;
@@ -273,6 +247,7 @@ namespace qualifyingwork
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- qualifyingwork/Form1.cs
+++ qualifyingwork/Form1.cs
@@ -18,17 +18,15 @@ namespace qualifyingwork
             InitializeComponent();
         }
 
+        uint[] data; //массив для хранения параметров типа uint
+        uint[] data2; //массив для хранения параметров типа uint
+        ushort size_memory; //размер памяти в байтах
+        ushort size_header; //размер заголовка в байтах
+
         private void button1_Click(object sender, EventArgs e)
         {
-            //Ввод выражения для записи в общую память
             char[] message = textBox1.Text.ToCharArray();
-            //Размер введенного сообщения
             int size = message.Length;
-
-            //Создание участка разделяемой памяти
-            //Первый параметр - название участка, 
-            //второй - длина участка памяти в байтах: тип char  занимает 2 байта 
-            //плюс четыре байта для одного объекта типа Integer
             MemoryMappedFile sharedMemory = MemoryMappedFile.CreateOrOpen("MemoryFile", size * 2 + 4);
             //Создаем объект для записи в разделяемый участок памяти
             using (MemoryMappedViewAccessor writer = sharedMemory.CreateViewAccessor(0, size * 2 + 4))
@@ -42,13 +40,8 @@ namespace qualifyingwork
         } //кнопка отправить
         private void button2_Click(object sender, EventArgs e)
         {
-            //Массив для сообщения из общей памяти
             char[] message2;
-            //Размер введенного сообщения
-            int size2;
-
-            //Получение существующего участка разделяемой памяти
-            //Параметр - название участка
+            int size2;    
             MemoryMappedFile sharedMemory = MemoryMappedFile.OpenExisting("MemoryFile");
             //Сначала считываем размер сообщения, чтобы создать массив данного размера
             //Integer занимает 4 байта, начинается с первого байта, поэтому передаем цифры 0 и 4
@@ -68,22 +61,13 @@ namespace qualifyingwork
                 reader.ReadArray<char>(0, message2, 0, size2);
             }
 
-            string msg = new string(message2);
-            textBox2.Text = msg;
+            string msg = new string(message2);           
             label2.Text = "Получено сообщение : " + msg;
         } //кнопка получить
-
-
-        private void button4_Click(object sender, EventArgs e)
-        {         
-            char[] message2;
-            ushort[] data;
-            ushort size_memory; //размер памяти
-            ushort size_header; //размер заголовка
-
+        private void button4_Click(object sender, EventArgs e) //кнопка получить параметры
+        {
+            
             MemoryMappedFile sharedMemory = MemoryMappedFile.OpenExisting("SIMITSharedMemory");
-            //Сначала считываем размер сообщения, чтобы создать массив данного размера
-            //Integer занимает 4 байта, начинается с первого байта, поэтому передаем цифры 0 и 4
             using (MemoryMappedViewAccessor reader = sharedMemory.CreateViewAccessor(0, 4, MemoryMappedFileAccess.Read))
             {
                 size_memory = reader.ReadUInt16(0);
@@ -94,23 +78,38 @@ namespace qualifyingwork
             }
             label7.Text = "Размер общей памяти: " + size_memory.ToString();
             label10.Text = "Размер заголовка: " + size_header.ToString(); 
-            //Считываем сообщение, используя полученный выше размер
-            //Сообщение - это строка или массив объектов char, каждый из которых занимает два байта
-            //Поэтому вторым параметром передаем число символов умножив на из размер в байтах плюс
-            //А первый параметр - смещение - 112 байта, которое занимает размер сообщения
-            using (MemoryMappedViewAccessor reader = sharedMemory.CreateViewAccessor(112, size_memory * 2, MemoryMappedFileAccess.Read))
-            {
-                //Массив символов сообщения               
-                data = new ushort[size_memory];
-                reader.ReadArray<ushort>(0, data, 0, size_memory);
+            using (MemoryMappedViewAccessor reader = sharedMemory.CreateViewAccessor(size_header, size_memory, MemoryMappedFileAccess.Read))
+            {             
+                data = new uint[size_memory];
+                reader.ReadArray<uint>(0, data, 0, size_memory);
             }
-
-  
             label5.Text = "1) " + data[0].ToString();
             label6.Text = "2) " + data[1].ToString();
-            label8.Text = "3) " + data[2].ToString();
-            label9.Text = "4) " + data[3].ToString();
-            
+            label8.Text = "3) " + data[2].ToString();       
+        }
+        private void button3_Click(object sender, EventArgs e) //кнопка отправить параметры
+        {
+            MemoryMappedFile sharedMemory = MemoryMappedFile.OpenExisting("SIMITSharedMemory");
+            using (MemoryMappedViewAccessor writer = sharedMemory.CreateViewAccessor(size_header + 12, size_memory, MemoryMappedFileAccess.Write))
+            {
+                data2 = new uint[size_memory - 12];
+                data2[0] = Convert.ToUInt32(textBox3.Text);
+                data2[1] = Convert.ToUInt32(textBox4.Text);
+                data2[2] = Convert.ToUInt32(textBox2.Text);
+                //writer.Write(0, 3);               
+                writer.WriteArray<uint>(size_header, data2, 12, 3);
+            }
+        }
+
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
